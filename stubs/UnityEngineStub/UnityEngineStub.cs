120242844cf81a8b1d363482ab28516a7b5db710
Givikenfkf <bigoboyscato@gmail.com>
2025-12-07 18:24:25 +0100

    Refactor UnityEngineStub with minimal implementations

--- stubs/UnityEngineStub/UnityEngineStub.cs
+++ stubs/UnityEngineStub/UnityEngineStub.cs
@@ -1,110 +1,75 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 
 namespace UnityEngine
 {
-    // Base "Object" so many Unity types can inherit from it (and be passed to Destroy).
-    public class Object { }
+    public class Object
+    {
+        public string name;
 
-    // Minimal Vector3
+        public static void Destroy(Object obj) { /* stub */ }
+        public static T Instantiate<T>(T obj) where T : Object => obj;
+    }
+
     public struct Vector3
     {
-        public float x,y,z;
-        public Vector3(float X, float Y, float Z) { x=X; y=Y; z=Z; }
+        public float x, y, z;
+        public Vector3(float x, float y, float z) { this.x = x; this.y = y; this.z = z; }
         public static Vector3 zero => new Vector3(0,0,0);
     }
 
-    // Minimal Quaternion
     public struct Quaternion
     {
-        public float x,y,z,w;
-        public Quaternion(float X,float Y,float Z,float W) { x=X; y=Y; z=Z; w=W; }
-        public static Quaternion identity => new Quaternion(0,0,0,1);
+        public float x, y, z, w;
+        public static Quaternion identity => new Quaternion();
     }
 
     public struct Color
     {
-        public float r,g,b,a;
-        public Color(float r,float g,float b,float a=1f){ this.r=r; this.g=g; this.b=b; this.a=a; }
+        public float r, g, b, a;
+        public static Color white => new Color { r=1, g=1, b=1, a=1 };
     }
 
-    // Texture2D inherits Object so it can be passed to Object.Destroy
-    public class Texture2D : Object
+    public class MonoBehaviour
     {
-        public Texture2D(int w,int h) {}
-        public void SetPixel(int x,int y, Color c) {}
-        public void Apply() {}
+        public void Invoke(string methodName, float time) { /* stub */ }
+        public void StartCoroutine(System.Collections.IEnumerator routine) { /* stub */ }
     }
 
-    public class ObjectHelpers
-    {
-        public static void Destroy(Object o) {}
-    }
-
-    // Keep the same static Destroy signature used in UnityEngine.Object
-    public static class ObjectEx
-    {
-        public static void Destroy(Object o) {}
-    }
-
-    // Minimal Transform
     public class Transform
     {
         public Vector3 position;
         public Quaternion rotation;
     }
 
-    // GameObject and Camera inherit from Object so they match Destroy param expectations
-    public class GameObject : Object
+    public class GameObject
     {
+        public string name;
         public Transform transform = new Transform();
-        public static GameObject Find(string name) => null;
+        public GameObject(string name = "") { this.name = name; }
     }
 
-    public class Camera : Object
+    public class Camera { }
+    public class Texture2D { }
+
+    public struct Rect
     {
-        public Transform transform = new Transform();
-        public static Camera main => null;
+        public float x, y, width, height;
     }
 
-    public static class Input
+    public class WaitForSeconds
     {
-        public static bool GetButton(string name) => false;
+        public WaitForSeconds(float seconds) { }
     }
 
-    // WaitForSeconds used as IEnumerator yield instruction
-    public class WaitForSeconds : IEnumerator
+    public static class Debug
     {
-        public WaitForSeconds(float seconds) {}
-        public object Current => null;
-        public bool MoveNext() => false;
-        public void Reset() {}
+        public static void Log(object message) { /* stub */ }
+        public static void LogWarning(object message) { /* stub */ }
+        public static void LogError(object message) { /* stub */ }
     }
 
     public static class GUI
     {
-        public static void DrawTexture(Rect r, Texture2D tex) {}
-    }
-
-    public struct Rect
-    {
-        public float x,y,width,height;
-        public Rect(float x,float y,float w,float h){ this.x=x; this.y=y; this.width=w; this.height=h; }
-    }
-
-    // Make MonoBehaviour a real class that exposes StartCoroutine and Destroy abilities.
-    public class MonoBehaviour : Object
-    {
-        public IEnumerator StartCoroutine(IEnumerator routine) { return routine; }
-        public void StopCoroutine(IEnumerator routine) { }
-    }
-
-    public class QuaternionWrapper { } // placeholder if referenced
-
-    // minimal Mathf
-    public static class Mathf
-    {
-        public static float Clamp01(float v) { return v < 0 ? 0 : (v > 1 ? 1 : v); }
+        public static void Label(Rect rect, string text) { /* stub */ }
     }
 }
